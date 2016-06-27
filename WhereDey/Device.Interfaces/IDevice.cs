cf02383d21fd4d2a1ed9e8c5bfb02307838e0254
greg hoover <greg.hoover@sungardps.com>
2016-06-26 23:24:44 -0400

    Allow access to Driod device location.

---
+++ WhereDey/Device.Interfaces/IDevice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using System.Device.Location;
+
+namespace Device.Interfaces
+{
+	/// <summary>
+	/// This interface defines the methods exposed by an actor.
+	/// Clients use this interface to interact with the actor that implements it.
+	/// </summary>
+	public interface IDevice : IActor
+	{
+		/// <summary>
+		/// TODO: Replace with your own actor method.
+		/// </summary>
+		/// <returns></returns>
+		Task<int> GetCountAsync();
+
+		/// <summary>
+		/// TODO: Replace with your own actor method.
+		/// </summary>
+		/// <param name="count"></param>
+		/// <returns></returns>
+		Task SetCountAsync(int count);
+
+		Task<GeoPosition<GeoCoordinate>> GetCurrentLocationAsync();
+		Task SetCurrentLocationAsync(GeoPosition<GeoCoordinate> loc);
+	}
+}
