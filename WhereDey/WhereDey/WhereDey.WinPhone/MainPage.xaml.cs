77801c4d9b1ba15e8fc3c796b6a5e005528daca9
greg hoover <greg.hoover@sungardps.com>
2016-06-27 02:55:08 -0400

    added geo plugin stuff

--- WhereDey/WhereDey/WhereDey.WinPhone/MainPage.xaml.cs
+++ WhereDey/WhereDey/WhereDey.WinPhone/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
--- WhereDey/WhereDey/WhereDey/App.cs
+++ WhereDey/WhereDey/WhereDey/App.cs
@@ -2,44 +2,77 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
 
 namespace WhereDey
 {
-    public class App : Application
-    {
-        public App()
-        {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                         new Label {
-                             HorizontalTextAlignment = TextAlignment.Center,
-                             Text = "Welcome to Xamarin Forms!"
-                         }
-                     }
-                }
-            };
-        }
+	public class App : Application
+	{
+		public App()
+		{
+			var buttonGetGPS = new Button
+			{
+				Text = "GetGPS"
+			};
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
+			var labelGPS = new Label
+			{
+				Text = "GPS goes here"
+			};
 
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
+			buttonGetGPS.Clicked += async (sender, args) =>
+			{
+				var locator = CrossGeolocator.Current;
+				locator.DesiredAccuracy = 50;
+				labelGPS.Text = "Getting gps";
 
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
-    }
+				var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+
+				if (position == null)
+				{
+					labelGPS.Text = "null gps :(";
+					return;
+				}
+				labelGPS.Text = string.Format("Time: {0} \nLat: {1} \nLong: {2} \n Altitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \n Heading: {6} \n Speed: {7}",
+				position.Timestamp, position.Latitude, position.Longitude,
+				position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
+			};
+
+			// The root page of your application
+			MainPage = new ContentPage
+			{
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						 new Label {
+							 HorizontalTextAlignment = TextAlignment.Center,
+							 Text = "Welcome to Xamarin Forms!"
+						 },
+						 buttonGetGPS,
+						 labelGPS
+					 }
+				}
+			};
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
 }
